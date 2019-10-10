03830a37ae3ad54f5cfae9c6a33868b031d5a42e
Mauro Lepore <maurolepore@gmail.com>
2019-10-10 09:42:29 -0300

    Fix typo

--- R/match_addin.R
+++ R/match_addin.R
@@ -3,11 +3,11 @@
 #' FIXME: This is work in progress. At this stage these functions do useless
 #' stuff. We use them only to test the interfaces they provide:
 #' * `match_addin()`:
-#'     * The user enter parameters on a pop-up window.
+#'     * The user enters parameters on a pop-up window.
 #'     * The app prints stuff on the R console.
 #'     * It must run from R or RStudio (can't be hosted online).
 #' * `match_app()`:
-#'     * The user enter parameters on the web browser.
+#'     * The user enters parameters on the web browser.
 #'     * The app prints stuff on the web browser.
 #'     * It can be hosted online, maybe on our private server.
 #'
