25604875a8c16706376c9b6b61c742e97b03c592
Mauro Lepore <maurolepore@gmail.com>
2019-10-14 10:31:56 -0300

    Use packages
    r2dii.dataraw dplyr and magrittr

--- R/gen_id.R
+++ R/gen_id.R
@@ -1,7 +1,7 @@
-#' Generate values to populate id_direct_loantaker, and id_ultimate_parent in the loanbook.csv
+#' Populate id_direct_loantaker, and id_ultimate_parent in the loanbook
 #'
-#' This function will generate identifiers for id_direct_loantaker and id_ultimate_parent that are
-#' unique to every (Name,Sector) pair.
+#' This function will generate identifiers for id_direct_loantaker and
+#' id_ultimate_parent that are unique to every (Name,Sector) pair.
 #'
 #' @param lbk The input loanbook. See r2ii.dataraw for details.
 #'
@@ -10,12 +10,22 @@
 #'
 #' @examples
 #' lbk_id_adjusted <- gen_id(lbk)
-
-library(tidyverse)
-gen_id <- function(lbk_input){
+gen_id <- function(lbk_input) {
   lbk_output <- lbk_input %>%
-    mutate(id_direct_loantaker =group_indices(.,name_direct_loantaker,sector_classification_direct_loantaker),
-           id_ultimate_parent = group_indices(.,name_ultimate_parent,sector_classification_direct_loantaker)) %>%
-    mutate(id_direct_loantaker = paste0("C",id_direct_loantaker),
-           id_ultimate_parent = paste0("UP", id_ultimate_parent))
+    mutate(
+      id_direct_loantaker = dplyr::group_indices(
+        .,
+        name_direct_loantaker,
+        sector_classification_direct_loantaker
+      ),
+      id_ultimate_parent = dplyr::group_indices(
+        .,
+        name_ultimate_parent,
+        sector_classification_direct_loantaker
+      )
+    ) %>%
+    mutate(
+      id_direct_loantaker = paste0("C", id_direct_loantaker),
+      id_ultimate_parent = paste0("UP", id_ultimate_parent)
+    )
 }
