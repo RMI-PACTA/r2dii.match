0ee8c50344d5cea760e586c97343be2f5ea07ba6
Jackson Hoffart <jackson.hoffart@gmail.com>
2019-10-18 17:19:54 +0200

    added first tests for sector_bridge.R

---
+++ tests/testthat/test-sector_bridge.R
@@ -0,0 +1,27 @@
+test_that("sector_bridge prints its output", {
+  out <- capture.output(sector_bridge(sector_bridge(r2dii.dataraw::loanbook_demo)))
+  expect_false(identical(out, character(0)))
+})
+
+test_that("sector_bridge returns a tibble dataframe", {
+  out <- sector_bridge(sector_bridge(r2dii.dataraw::loanbook_demo))
+  expect_is(out, "tbl_df")
+})
+
+test_that("sector_bridge errs gracefully with wrong input", {
+
+  invalidate <- function(data, x) {
+    dplyr::rename(data, bad = x)
+  }
+
+  lbk <- r2dii.dataraw::loanbook_demo
+  expect_error(
+    sector_bridge(invalidate(lbk, "name_direct_loantaker")),
+    "must have.*sector_classification_system"
+  )
+  expect_error(
+    sector_bridge(invalidate(lbk, "sector_classification_direct_loantaker")),
+    "must have.*sector_classification_direct_loantaker"
+  )
+
+})
