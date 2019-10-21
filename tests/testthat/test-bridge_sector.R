7fc0f306bdd7fe3f4c7316a167557194704a3603
Mauro Lepore <maurolepore@gmail.com>
2019-10-21 10:11:02 -0300

    New `bidge_sector()` classification (#7, #9; thanks @jdhoffa)
    Closes #7 and closes #9
    
    Details:
    
    * Reflow to fit within 80 characters
    * Fix R CMD check.
    * Check crucial names.
    * Add a bunch of tests.
    * Refactor intensively.
    
    Thanks @jdhoffa 
    Signed-off-by: Mauro Lepore <maurolepore@gmail.com>

---
+++ tests/testthat/test-bridge_sector.R
@@ -0,0 +1,55 @@
+# bridge_sector() needs r2dii.dataraw in the search() path
+library(r2dii.dataraw)
+
+test_that("bridge_sector outputs known output", {
+  out <- bridge_sector(r2dii.dataraw::loanbook_demo)
+  expect_known_output(out, "ref-bridge_sector", update = FALSE)
+})
+
+test_that("bridge_sector returns a tibble dataframe", {
+  out <- bridge_sector(r2dii.dataraw::loanbook_demo)
+  expect_is(out, "tbl_df")
+})
+
+test_that("bridge_sector with wrong input errs gracefully", {
+  rename_crucial_column <- function(data, x) {
+    dplyr::rename(data, bad = x)
+  }
+  lbk <- r2dii.dataraw::loanbook_demo
+
+  lbk_missing_sector_classification_system <-
+    rename_crucial_column(lbk, "sector_classification_system")
+  expect_error(
+    bridge_sector(lbk_missing_sector_classification_system),
+    "must have.*sector_classification_system"
+  )
+
+  lbk_missing_sector_classification_direct_loantaker <-
+    rename_crucial_column(lbk, "sector_classification_direct_loantaker")
+  expect_error(
+    bridge_sector(lbk_missing_sector_classification_direct_loantaker),
+    "must have.*sector_classification_direct_loantaker"
+  )
+})
+
+test_that("bridge_sector adds two columns: `sector` and `borderline`", {
+  input <- r2dii.dataraw::loanbook_demo
+  expect_false(hasName(input, "sector"))
+  expect_false(hasName(input, "borderline"))
+
+  output <- bridge_sector(input)
+  new_columns <- sort(setdiff(names(output), names(input)))
+  expect_equal(
+    new_columns, c("borderline", "sector")
+  )
+})
+
+test_that("bridge_sector preserves typeof() input columns", {
+  input <- r2dii.dataraw::loanbook_demo
+  output <- bridge_sector(input)
+
+  expect_equal(
+    purrr::map_chr(input, typeof),
+    purrr::map_chr(output[names(input)], typeof),
+  )
+})
