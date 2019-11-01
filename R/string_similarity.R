c4e1721c6a24b4ca1c43f3181d16611be8aa48a4
Mauro Lepore <maurolepore@gmail.com>
2019-11-01 15:14:09 -0300

    New string_similarity()
    Closes #18.
    
    New string_similarity() wraps`stringdist::stringsim() to
    provide defalts for most common 2dii analyses.
    
    Also:
    
    * Use ellipsis to check dots used:
      We now use ellipsis to ensure misspelled arguments passed
      to ... throw an explicit error. This approach is now used
      on devtools (since 2.1.1; see
      https://www.tidyverse.org/articles/2019/09/devtools-2-2-1/)
    * Style, document, spellcheck.
    
    Thanks @jdhoffa

---
+++ R/string_similarity.R
@@ -0,0 +1,36 @@
+#' Compute similarity scores between strings
+#'
+#' This function computes pairwise string similarities between elements of
+#' character vectors `a` and `b`, where the vector with less elements is
+#' recycled. It is a thin wrapper around `stringdist::stringdist()` with
+#' defaults set to the most common parameters used in 2dii analyses.
+#'
+#' @inherit stringdist::stringsim
+#'
+#' @param method Method for distance calculation. One of `c("osa", "lv", "dl",
+#'   "hamming", "lcs", "qgram", "cosine", "jaccard", "jw", "soundex")`. See
+#'   [stringdist::stringdist-metrics].
+#' @inheritParams stringdist::stringdist
+#' @param ... Additional arguments are passed on to [stringdist::stringsim].
+#'
+#' @seealso [stringdist::stringsim], [stringdist::stringdist].
+#'
+#' @export
+#' @examples
+#' # Clear extreemes
+#' string_similarity("aa", "aa")
+#' string_similarity("aa", "bb")
+#'
+#' # Unclear extreemes
+#' string_similarity("ab", "ba")
+#'
+#' identical(
+#'   string_similarity("ab", "ac"),
+#'   string_similarity("ac", "ab")
+#' )
+#'
+#' string_similarity(c("fewer", "items", "get", "recycled"), "recycled")
+string_similarity <- function(a, b, method = "jw", p = 0.1, ...) {
+  ellipsis::check_dots_used()
+  stringdist::stringsim(a = a, b = b, method = method, p = p, ...)
+}
---
+++ tests/testthat/test-string_similarity.R
@@ -0,0 +1,74 @@
+test_that("string_similarity with clearly extreeme cases returns 0 or 1", {
+  expect_equal(string_similarity("aa", "aa"), 1L)
+  expect_equal(string_similarity("aa", "bb"), 0L)
+})
+
+test_that("string_similarity cares not for the order of the string-elements", {
+  expect_true(
+    identical(
+      string_similarity("ab", "ac"),
+      string_similarity("ac", "ab")
+    )
+  )
+})
+
+test_that("string_similarity output is as long as its longer input", {
+  out <- string_similarity(
+    c("long", "vector"),
+    "short"
+  )
+  expect_length(out, 2L)
+
+  out <- string_similarity(
+    "short",
+    c("long", "vector")
+  )
+  expect_length(out, 2L)
+})
+
+test_that("string_similariry is sensitive to `method`", {
+  method_osa <- string_similarity("abc", "acb", method = "osa")
+  method_default <- string_similarity("abc", "acb")
+  expect_false(identical(method_osa, method_default))
+})
+
+test_that("string_similariry is sensitive to `p`", {
+  p_default <- string_similarity("abc", "acb")
+  p_custom <- string_similarity("abc", "acb", p = 0.25)
+  expect_false(identical(p_custom, p_default))
+})
+
+test_that("string_similariry passes arguments to stringdist via `...`", {
+  x <- y <- letters[1:10]
+  default <- string_similarity(x, y)
+
+  weight <- seq(0.1, 0.3, by = 0.1)
+  custom_weight <- string_similarity(x, y, weight = weight)
+  expect_false(identical(default, custom_weight))
+})
+
+test_that("string_similariry errors with misspelled argument passed to `...`", {
+  x <- y <- letters[1:10]
+  weights <- seq(0.1, 0.3, by = 0.1)
+
+  expect_error(
+    string_similarity(x, y, method = "jw", p = 0.1, weight = weights),
+    NA
+  )
+
+  # Misspelled weight as weig(th)
+  expect_error(
+    string_similarity(x, y, method = "jw", p = 0.1, weigth = weights),
+    "problematic.*weigth",
+    class = "rlib_error_dots_unused"
+  )
+})
+
+test_that("string_similariry errors with misspelled `method`", {
+  expect_error(string_similarity("a", "a", method = "jw"), NA)
+  expect_error(
+    string_similarity("a", "a", metod = "jw"),
+    "problematic.*metod",
+    class = "rlib_error_dots_unused"
+  )
+})
