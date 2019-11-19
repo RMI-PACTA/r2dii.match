9c64361edaa38863be0fbc71b74c51a88e62cfdd
Mauro Lepore <maurolepore@gmail.com>
2019-11-19 12:29:32 -0300

    No longer use %|% (follow up 5b06ed2)

--- R/imports.R
+++ R/imports.R
@@ -1,7 +1,7 @@
 #' @importFrom r2dii.utils check_crucial_names
 #' @importFrom dplyr mutate tibble select if_else
 #' @importFrom purrr reduce
-#' @importFrom rlang %||% %|%
+#' @importFrom rlang %||%
 #' @importFrom usethis ui_oops ui_field
 NULL
 
