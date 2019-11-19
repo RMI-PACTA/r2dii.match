fc578078f2728a0f3a5d9a75136d88023e19792d
Mauro Lepore <maurolepore@gmail.com>
2019-11-18 21:40:45 -0300

    Refactor lightly, mostly for style
    * Fit documentation within 80-characters lines
      Set a reference line at 80 characters in RStudio with:
      Tools > Global Options > Code > Display > Show margin >
      Margin column = 80
    * Refactor examples
      Just a matter of taste. The code before was perfectly fine.
    * usethis::use_tidy_description()
    * styler::style_pkg()

--- R/prepare_loanbook_for_matching.R
+++ R/prepare_loanbook_for_matching.R
@@ -4,21 +4,21 @@
 #' preparing it for the fuzzy matching process.
 #'
 #' @param data A sector bridged loanbook dataframe.
-#' @param overwrite A dataframe used to overwrite the sector and/or name of a particular loan,
-#' direct loantaker or ultimate parent. Elements to be overwritten are identified by any one of
-#' their IDs (loan, direct loantaker or ultimate parent).
+#' @param overwrite A dataframe used to overwrite the sector and/or name of a
+#'   particular loan, direct loantaker or ultimate parent. Elements to be
+#'   overwritten are identified by any one of their IDs (loan, direct loantaker
+#'   or ultimate parent).
 #'
-#' @return A dataframe of all unique name + sector combinations, including all IDs, and with elements already manually overwritten.
+#' @return A dataframe of all unique name + sector combinations, including all
+#'   IDs, and with elements already manually overwritten.
 #' @export
 #'
 #' @examples
 #' library(r2dii.dataraw)
 #'
-#' input_loanbook <- r2dii.dataraw::loanbook_demo
-#'
-#' sector_bridged_loanbook <- r2dii.match::bridge_sector(input_loanbook)
-#'
-#' prepare_loanbook_for_matching(sector_bridged_loanbook)
+#' loanbook_demo %>%
+#'   bridge_sector() %>%
+#'   prepare_loanbook_for_matching()
 prepare_loanbook_for_matching <- function(data, overwrite = NULL) {
 
   # check that input data has all pertinent columns
@@ -34,11 +34,11 @@ prepare_loanbook_for_matching <- function(data, overwrite = NULL) {
   # initialize empty overwrite data.frame with necessary columns
   init_overwrite <- function() {
     data.frame(
-      level            = character(),
-      id               = character(),
-      name             = character(),
-      sector           = character(),
-      source           = character(),
+      level = character(),
+      id = character(),
+      name = character(),
+      sector = character(),
+      source = character(),
       stringsAsFactors = F
     )
   }
@@ -59,35 +59,42 @@ prepare_loanbook_for_matching <- function(data, overwrite = NULL) {
 
   # extract all unique id & name pairs, with corresponding level and sector
   loanbook_match_values <-
-    data %>% dplyr::select(
+    data %>%
+    dplyr::select(
       .data$id_direct_loantaker,
       .data$name_direct_loantaker,
       .data$id_ultimate_parent,
       .data$name_ultimate_parent,
       .data$sector
-    ) %>% tidyr::pivot_longer(
+    ) %>%
+    tidyr::pivot_longer(
       cols = tidyr::starts_with("id_"),
       names_to = "level",
       names_prefix = "id_",
       values_to = "id"
     ) %>%
-    dplyr::mutate(name =
-             ifelse(.data$level == "direct_loantaker", .data$name_direct_loantaker, NA)) %>%
-    dplyr::mutate(name =
-             ifelse(.data$level == "ultimate_parent", .data$name_ultimate_parent, .data$name)) %>%
+    dplyr::mutate(
+      name =
+        ifelse(.data$level == "direct_loantaker", .data$name_direct_loantaker, NA)
+    ) %>%
+    dplyr::mutate(
+      name =
+        ifelse(.data$level == "ultimate_parent", .data$name_ultimate_parent, .data$name)
+    ) %>%
     dplyr::mutate(source = "loanbook") %>%
     dplyr::select(.data$level, .data$id, .data$name, .data$sector, .data$source) %>%
     dplyr::distinct()
 
   # join in manual values
-  loanbook_match_values_overwrite <- dplyr::left_join(loanbook_match_values, overwrite, by=c("id", "level")) %>%
-    dplyr::mutate(name = ifelse(is.na(.data$name.y), .data$name.x, .data$name.y),
-           sector = ifelse(is.na(.data$sector.y), .data$sector.x, .data$sector.y),
-           source = ifelse(is.na(.data$source.y), .data$source.x, "manual")) %>%
+  loanbook_match_values_overwrite <- dplyr::left_join(loanbook_match_values, overwrite, by = c("id", "level")) %>%
+    dplyr::mutate(
+      name = ifelse(is.na(.data$name.y), .data$name.x, .data$name.y),
+      sector = ifelse(is.na(.data$sector.y), .data$sector.x, .data$sector.y),
+      source = ifelse(is.na(.data$source.y), .data$source.x, "manual")
+    ) %>%
     dplyr::select(.data$level, .data$id, .data$name, .data$sector, .data$source)
 
   # simplify name
   loanbook_match_values_overwrite %>%
     dplyr::mutate(simplified_name = r2dii.match::replace_customer_name(.data$name))
-
 }
