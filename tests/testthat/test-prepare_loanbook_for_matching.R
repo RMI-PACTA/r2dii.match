96cb79f0f5431e47625643b7d3f7cbdcbc5894e6
Mauro Lepore <maurolepore@gmail.com>
2019-11-18 21:30:12 -0300

    prepare_loanbook_for_matching() outputs a tibble
    Also little tidy

--- tests/testthat/test-prepare_loanbook_for_matching.R
+++ tests/testthat/test-prepare_loanbook_for_matching.R
@@ -1,6 +1,19 @@
-test_that("ensure prepared loanbook has no duplicated ID values", {
-  out <- prepare_loanbook_for_matching(r2dii.match::bridge_sector(r2dii.dataraw::loanbook_demo))
+library(r2dii.dataraw)
+
+test_that("prepare_loanbook_for_matching outputs unique `id` values", {
+  out <- prepare_loanbook_for_matching(
+    bridge_sector(loanbook_demo)
+  )
+
   expect_false(
     any(duplicated(out$id))
   )
+})
+
+test_that("prepare_loanbook_for_matching outputs a tibble", {
+  out <- prepare_loanbook_for_matching(
+    bridge_sector(loanbook_demo)
+  )
+
+  expect_is(out, "tbl_df")
 })
