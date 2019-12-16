adc5ebbcf0a0b9e6038da499410ddbe9a1267ebb
Mauro Lepore <maurolepore@gmail.com>
2019-12-16 10:48:17 -0600

    * Rename to match_by_simpler_name
    To more closely reflect requirements by Klaus

--- R/match_by_simpler_name.R
+++ R/match_by_simpler_name.R
@@ -1,10 +1,10 @@
-match_loanbook_with_ald <- function(x,
-                                    y,
-                                    ...,
-                                    by_sector = TRUE,
-                                    threshold = 0.8,
-                                    method = "jw",
-                                    p = 0.1) {
+match_by_simpler_name <- function(x,
+                                  y,
+                                  ...,
+                                  by_sector = TRUE,
+                                  threshold = 0.8,
+                                  method = "jw",
+                                  p = 0.1) {
   matched <- match_all_against_all(x, y, by_sector = FALSE)
 
   with_sector_x <- matched %>%
--- tests/testthat/test-match_by_simpler_name.R
+++ tests/testthat/test-match_by_simpler_name.R
@@ -1,16 +1,16 @@
 library(dplyr)
 
-test_that("match_loanbook_with_ald takes `threshold`", {
+test_that("match_by_simpler_name takes `threshold`", {
   x <- tibble(sector = c("A", "B", "B"), simpler_name = c("xa", "xb", "xc"))
   y <- tibble(sector = c("A", "B", "C"), simpler_name = c("ya", "yb", "yc"))
 
   expect_error(
-    match_loanbook_with_ald(x, y, threshold = 0.5),
+    match_by_simpler_name(x, y, threshold = 0.5),
     NA
   )
 })
 
-test_that("match_loanbook_with_ald takes `by_sector`", {
+test_that("match_by_simpler_name takes `by_sector`", {
   x <- tibble(sector = c("A", "B", "B"), simpler_name = c("xa", "xb", "xc"))
   y <- tibble(sector = c("A", "B", "C"), simpler_name = c("ya", "yb", "yc"))
 
@@ -19,39 +19,39 @@ test_that("match_loanbook_with_ald takes `by_sector`", {
   expect_false(identical(out1, out2))
 })
 
-test_that("match_loanbook_with_ald has all formals in match_all_against_all", {
-  actual <- names(formals("match_loanbook_with_ald"))
+test_that("match_by_simpler_name has all formals in match_all_against_all", {
+  actual <- names(formals("match_by_simpler_name"))
   expected <- names(formals("match_all_against_all"))
   expect_equal(setdiff(expected, actual), character(0))
 })
 
-test_that("match_loanbook_with_ald recovers `sector_x`", {
+test_that("match_by_simpler_name recovers `sector_x`", {
   x <- tibble(sector = c("A", "B", "B"), simpler_name = c("xa", "xb", "xc"))
   y <- tibble(sector = c("A", "B", "C"), simpler_name = c("ya", "yb", "yc"))
 
   expect_true(
-    rlang::has_name(match_loanbook_with_ald(x, y), "sector_x")
+    rlang::has_name(match_by_simpler_name(x, y), "sector_x")
   )
 })
 
-test_that("match_loanbook_with_ald recovers `sector_y`", {
+test_that("match_by_simpler_name recovers `sector_y`", {
   x <- tibble(sector = c("A", "B", "B"), simpler_name = c("xa", "xb", "xc"))
   y <- tibble(sector = c("A", "B", "C"), simpler_name = c("ya", "yb", "yc"))
 
   expect_true(
-    rlang::has_name(match_loanbook_with_ald(x, y), "sector_y")
+    rlang::has_name(match_by_simpler_name(x, y), "sector_y")
   )
 })
 
-test_that("match_loanbook_with_ald has all columns in loanbook and ald", {
+test_that("match_by_simpler_name has all columns in loanbook and ald", {
   x <- tibble(lbk_column = "col",  sector = "a", simpler_name = "xa")
   y <- tibble(ald_column = "col",  sector = "a", simpler_name = "ya")
 
   expect_true(
-    rlang::has_name(match_loanbook_with_ald(x, y), "lbk_column")
+    rlang::has_name(match_by_simpler_name(x, y), "lbk_column")
   )
 
   expect_true(
-    rlang::has_name(match_loanbook_with_ald(x, y, threshold = 0), "ald_column")
+    rlang::has_name(match_by_simpler_name(x, y, threshold = 0), "ald_column")
   )
 })
