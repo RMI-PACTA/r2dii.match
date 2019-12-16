cb274bf1fef11cec2a2b8e273c460674ee5a200f
Mauro Lepore <maurolepore@gmail.com>
2019-12-16 07:26:06 -0600

    * Takes `by_sector`

--- tests/testthat/test-match_loanbook_with_ald.R
+++ tests/testthat/test-match_loanbook_with_ald.R
@@ -19,9 +19,30 @@ test_that("match_loanbook_with_ald takes `threshold`", {
   )
 })
 
-# test_that("match_loanbook_with_ald recovers `sector`", {
-#   library(dplyr)
+test_that("match_loanbook_with_ald takes `by_sector`", {
+  x <- tibble(sector = c("A", "B", "B"), simpler_name = c("xa", "xb", "xc"))
+  y <- tibble(sector = c("A", "B", "C"), simpler_name = c("ya", "yb", "yc"))
+
+  out1 <- match_all_against_all(x, y, by_sector = TRUE)
+  out2<- match_all_against_all(x, y, by_sector = FALSE)
+  expect_false(identical(out1, out2))
+})
 #
+# test_that("match_loanbook_with_ald recovers `sector`", {
+#   x <- tibble(sector = c("A", "B", "B"), simpler_name = c("xa", "xb", "xc"))
+#   y <- tibble(sector = c("A", "B", "C"), simpler_name = c("ya", "yb", "yc"))
+#
+#   out <- match_all_against_all(x, y, by_sector = FALSE)
+#   out
+#
+#   # Recover sectors from x & y
+#   left_join(out, x, by = c("simpler_name_x" = "simpler_name")) %>%
+#     dplyr::rename(sector_x = sector) %>%
+#     left_join(y, by = c("simpler_name_y" = "simpler_name")) %>%
+#     dplyr::rename(sector_y = sector)
+#
+# })
+# test_that("match_loanbook_with_ald recovers `sector`", {
 #   x <- tibble(sector = c("A", "B", "B"), simpler_name = c("xa", "xb", "xc"))
 #   y <- tibble(sector = c("A", "B", "C"), simpler_name = c("ya", "yb", "yc"))
 #
