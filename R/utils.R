0936a1bff80045ecb204d8810d183491cb937cf4
Mauro Lepore <maurolepore@gmail.com>
2020-01-10 15:23:56 -0600

    Explicit test (#93)
    * Test explicit data and new helpers for mini data
    
    * Test match_name yields some non-na in lbk cols
    
    Relates to #85, closed but not yet tested.
    Thanks @jdhoffa

--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,50 @@
     x
   }
 }
+
+# Helpers to create mini data ---------------------------------------------
+
+# mini_lbk(loanbook_demo, 1)
+mini_lbk <- function(loanbook, ..., vars = crucial_lbk()) {
+  loanbook %>%
+    dplyr::slice(...) %>%
+    dplyr::select(vars, tidyselect::matches("intermediate"))
+}
+
+# loanbook_demo %>%
+#   mini_lbk(1) %>%
+#   mini_ald()
+mini_ald <- function(loanbook, alias_ald = alias_ald()) {
+  alias_ald %>%
+    filter(.data$alias_ald %in% pull_alias_ald(loanbook, .))
+}
+
+# alias_ald()
+alias_ald <- function() {
+  r2dii.dataraw::ald_demo %>%
+    dplyr::select(crucial_ald()) %>%
+    dplyr::mutate(alias_ald = to_alias(.data$name_company)) %>%
+    unique()
+}
+
+pull_alias_ald <- function(loanbook, ald) {
+  loanbook %>%
+    match_name(ald) %>%
+    dplyr::select(dplyr::ends_with("ald")) %>%
+    dplyr::pull(.data$alias_ald)
+}
+
+crucial_lbk <- function() {
+  c(
+    "id_ultimate_parent",
+    "name_ultimate_parent",
+    "id_direct_loantaker",
+    "name_direct_loantaker",
+    "sector_classification_system",
+    "sector_classification_direct_loantaker"
+  )
+}
+
+crucial_ald <- function() {
+  c("name_company", "sector")
+}
