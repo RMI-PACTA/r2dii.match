2e5ab0056c6ef0d69441de44fbce3548ead16322
Mauro Lepore <maurolepore@gmail.com>
2020-01-13 10:38:54 -0600

    Refactor: Reuse dplyr::select()

--- R/utils.R
+++ R/utils.R
@@ -7,11 +7,9 @@
 }
 
 extract_level_names <- function(data, prefix) {
-  extract_names(data, paste0(prefix, level(), collapse = "|"))
-}
-
-extract_names <- function(x, pattern) {
-  grep(pattern, names(x), value = TRUE)
+  pattern <- paste0(prefix, level(), collapse = "|")
+  columns <- select(data, matches(pattern))
+  names(columns)
 }
 
 level <- function() {
