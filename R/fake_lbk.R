5931b8bb4ce2212c4a1552cfcba6aa62c60f3432
Mauro Lepore <maurolepore@gmail.com>
2020-01-24 06:28:48 -0600

    Rename file to reflect most visible functions
    The fake_*() variants are called in tests and leave a more visible
    record of their usage than the mini_*() variants. Thus, someone
    looking for this file will most likely go after seeing the fake_*()
    variants.

--- R/fake_lbk.R
+++ R/fake_lbk.R
@@ -1,12 +1,70 @@
-#' Help to create minimal loanbook data
+#' Minimal explicit loanbook and ald datasets that allow overwriting values
 #'
-#' This function helps to create a minimal loanbook and ald datasets:
+#' These funtions are developer-oriented:
+#' * `fake_lbk()` is based on `mini_lbk(loanbook_demo, 1)` with the additional,
+#'   non-crucal columns `id_intermediate_parent_1`, `name_intermediate_parent_1`.
+#' * `fake_lbk()` is created from applying `mini_lbk()` to `fake_lbk()`.
+#'
+#' @section Params:
+#' The arguments are the names of the crucial columns of loanbook and ald
+#' datasets.
+#'
+#' @seealso `mini_lbk()` `mini_ald()`
+#'
+#' @return A dataframe
+#'
+#' @examples
+#' fake_lbk()
+#'
+#' fake_ald()
+#'
+#' identical(fake_ald(), mini_ald(fake_lbk()))
+#'
+#' # Helps invalidate values for tests
+#' fake_ald(name_company = "bad")
+#' @noRd
+fake_lbk <- function(sector_classification_system = NULL,
+                     id_ultimate_parent = NULL,
+                     name_ultimate_parent = NULL,
+                     id_direct_loantaker = NULL,
+                     name_direct_loantaker = NULL,
+                     sector_classification_direct_loantaker = NULL) {
+  tibble::tibble(
+    sector_classification_system = sector_classification_system %||% "NACE",
+    id_ultimate_parent = id_ultimate_parent %||% "UP15",
+    name_ultimate_parent =
+      name_ultimate_parent %||% "Alpine Knits India Pvt. Limited",
+    id_direct_loantaker = id_direct_loantaker %||% "C294",
+    name_direct_loantaker =
+      name_direct_loantaker %||% "Yuamen Xinneng Thermal Power Co Ltd",
+    sector_classification_direct_loantaker =
+      sector_classification_direct_loantaker %||% 3511
+  )
+}
+
+#' See `fake_lbk()`
+#' @noRd
+fake_ald <- function(name_company = NULL,
+                     sector = NULL,
+                     alias_ald = NULL) {
+  tibble::tibble(
+    name_company = name_company %||% "alpine knits india pvt. limited",
+    sector = sector %||% "power",
+    alias_ald = alias_ald %||% "alpineknitsindiapvt ltd"
+  )
+
+}
+
+#' Help to create minimal loanbook (lbk) and asset-level data (ald)
+#'
+#' These functions are developer-oriented. They help you create minimal loanbook
+#' and ald datasets:
 #' * `mini_lbk()` allows you to focus on a particular slice of rows and, by
 #' default, focuses on the crucial columns for `match_name()`.
 #' * `mini_ald()` allows you to reverse-engineer the minimum piece of ald data
 #' you need to get the output of `match_name()` with a given loanbook data.
 #'
-#' You can transform the output of these functions into an explicit, text-based
+#' You may transform the output of these functions into an explicit, text-based
 #' dataframe by piping the output of this function to functions such as
 #' [dput()], `datapaste::df_paste()`, or `datapasta::tribble_paste()`.
 #'
@@ -26,6 +84,10 @@
 #'
 #' @examples
 #' mini_lbk(loanbook_demo, 1)
+#'
+#' loanbook_demo %>%
+#'   mini_lbk(1) %>%
+#'   mini_ald()
 #' @noRd
 mini_lbk <- function(loanbook, ..., vars = crucial_lbk()) {
   loanbook %>%
@@ -33,12 +95,7 @@ mini_lbk <- function(loanbook, ..., vars = crucial_lbk()) {
     dplyr::select(vars, tidyselect::matches("intermediate"))
 }
 
-#' See mini_lbk
-#'
-#' @examples
-#' loanbook_demo %>%
-#'   mini_lbk(1) %>%
-#'   mini_ald()
+#' See `mini_lbk()`
 #' @noRd
 mini_ald <- function(loanbook, alias_ald = NULL) {
   alias_ald <- alias_ald %||% alias_ald()
@@ -77,57 +134,3 @@ crucial_ald <- function() {
   c("name_company", "sector")
 }
 
-#' Minimal explicit loanbook and ald datasets that allow overwriting values
-#'
-#' * `fake_lbk()` is based on `mini_lbk(loanbook_demo, 1)` with the additional,
-#'   non-crucal columns `id_intermediate_parent_1`, `name_intermediate_parent_1`.
-#' * `fake_lbk()` is created from applying `mini_lbk()` to `fake_lbk()`.
-#'
-#' @section Params:
-#' The arguments are the names of the crucial columns of loanbook and ald
-#' datasets.
-#'
-#' @seealso `mini_lbk()` `mini_ald()`
-#'
-#' @return A dataframe
-#'
-#' @examples
-#' fake_lbk()
-#'
-#' fake_ald()
-#'
-#' identical(fake_ald(), mini_ald(fake_lbk()))
-#'
-#' # Helps invalidate values for tests
-#' fake_ald(name_company = "bad")
-#' @noRd
-fake_lbk <- function(sector_classification_system = NULL,
-                          id_ultimate_parent = NULL,
-                          name_ultimate_parent = NULL,
-                          id_direct_loantaker = NULL,
-                          name_direct_loantaker = NULL,
-                          sector_classification_direct_loantaker = NULL) {
-  tibble::tibble(
-    sector_classification_system = sector_classification_system %||% "NACE",
-    id_ultimate_parent = id_ultimate_parent %||% "UP15",
-    name_ultimate_parent =
-      name_ultimate_parent %||% "Alpine Knits India Pvt. Limited",
-    id_direct_loantaker = id_direct_loantaker %||% "C294",
-    name_direct_loantaker =
-      name_direct_loantaker %||% "Yuamen Xinneng Thermal Power Co Ltd",
-    sector_classification_direct_loantaker =
-      sector_classification_direct_loantaker %||% 3511
-  )
-}
-
-#' @noRd
-fake_ald <- function(name_company = NULL,
-                          sector = NULL,
-                          alias_ald = NULL) {
-  tibble::tibble(
-    name_company = name_company %||% "alpine knits india pvt. limited",
-    sector = sector %||% "power",
-    alias_ald = alias_ald %||% "alpineknitsindiapvt ltd"
-  )
-
-}
