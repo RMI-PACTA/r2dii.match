0fd10bb07f5ded60c8581efd2e426a5f8d15b560
Mauro Lepore <maurolepore@gmail.com>
2020-05-06 06:36:22 -0500

    Document `ald` may be like r2dii.data::ald_scenario_demo

--- R/match_name.R
+++ R/match_name.R
@@ -10,8 +10,9 @@
 #' @template alias-assign
 #' @template ignores-but-preserves-existing-groups
 #'
-#' @param loanbook,ald data frames structured like [r2dii.data::loanbook_demo]
-#'   and [r2dii.data::ald_demo].
+#' @param loanbook Data frame structured like [r2dii.data::loanbook_demo].
+#' @param ald Data frame structured like [r2dii.data::ald_demo] or
+#'   or [r2dii.data::ald_scenario_demo].
 #' @param by_sector Should names only be compared if companies belong to the same
 #'   `sector`?
 #' @param min_score A number between 0-1, to set the minimum `score` threshold.
