a5b5c64fe8323c9664e12df7421445e8edb0e489
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-12-01 18:11:54 +0100

    Fix `match_name` output if sector and borderline provided (#330) (#333)
    Relates to #330
    
    Co-authored-by: Mauro Lepore <maurolepore@gmail.com>

--- tests/testthat/test-match_name.R
+++ tests/testthat/test-match_name.R
@@ -629,10 +629,22 @@ test_that("with relevant options allows loanbook with reserved columns", {
   restore <- options(r2dii.match.allow_reserved_columns = TRUE)
   on.exit(options(restore), add = TRUE)
 
-  # Must add both `sector` and `borderline` -- match_name errors with just one
   lbk <- mutate(fake_lbk(), sector = "a", borderline = FALSE)
   expect_no_error(
     # Don't warn if found no match
     suppressWarnings(match_name(lbk, fake_ald()))
   )
+})
+
+test_that("with loanbook with(out) reserved columns, outputs same names", {
+  restore <- options(r2dii.match.allow_reserved_columns = TRUE)
+  on.exit(options(restore), add = TRUE)
+
+  reserved <- mutate(fake_lbk(), sector = "power", borderline = FALSE)
+  actual <- sort(names(match_name(reserved, fake_ald())))
+
+  standard <- fake_lbk()
+  expected <- sort(names(match_name(standard, fake_ald())))
+
+  expect_equal(actual, expected)
 })
