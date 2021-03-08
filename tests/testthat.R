ead765c4cb0383d98b63282b78f99e5a02148c3d
Mauro Lepore <maurolepore@gmail.com>
2021-03-07 20:05:56 -0800

    Move options() where testthat can see them

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,4 @@
 library(testthat)
 library(r2dii.match)
-options(tibble.width=60)
 
 test_check("r2dii.match")
---
+++ tests/testthat/helper-tibble.R
@@ -0,0 +1,2 @@
+# Avoid failure on CI due to difference in print width between local vs. CI
+options(tibble.width = 60)
