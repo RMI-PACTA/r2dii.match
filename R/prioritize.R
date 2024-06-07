b790f052325c176f297a89ef673b9e19d4837634
Alex Axthelm <aaxthelm@RMI.org>
2024-06-07 09:46:22 +0200

    Move external markdown reference to inline
    Thanks to @jdhoffa for the catch.

--- R/prioritize.R
+++ R/prioritize.R
@@ -21,7 +21,29 @@
 #'
 #' @details
 #' **How to validate `data`**
-#' @includeRmd vignettes/_validate-matches.md
+#' Write the output of `match_name()` into a .csv file with:
+#' 
+#' ```
+#' # Writting to current working directory
+#' matched %>%
+#'   readr::write_csv("matched.csv")
+#' ```
+#' 
+#' Compare, edit, and save the data manually:
+#' 
+#' * Open _matched.csv_ with any spreadsheet editor (Excel, Google Sheets, etc.).
+#' * Compare the columns `name` and `name_abcd` manually to determine if the match is valid. Other information can be used in conjunction with just the names to ensure the two entities match (sector, internal information on the company structure, etc.)
+#' * Edit the data:
+#'     * If you are happy with the match, set the `score` value to `1`.
+#'     * Otherwise set or leave the `score` value to anything other than `1`.
+#' * Save the edited file as, say, _valid_matches.csv_.
+#' 
+#' Re-read the edited file (validated) with:
+#' 
+#' ```
+#' # Reading from current working directory
+#' valid_matches <- readr::read_csv("valid_matches.csv")
+#' ```
 #'
 #' @return A data frame with a single row per loan, where `score` is 1 and
 #'   priority level is highest.
