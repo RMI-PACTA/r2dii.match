defdf00fcd3d73369b391b5e57efebd898b2543d
CJ Yetman <cj@cjyetman.com>
2025-02-05 16:57:38 +0100

    add `R/data_dictionary.R` for documentation

---
+++ R/data_dictionary.R
@@ -0,0 +1,18 @@
+#' Data Dictionary
+#'
+#' A table of column names and descriptions of data frames used or exported by
+#' the functions in this package.
+#'
+#' @format ## `data_dictionary`
+#' \describe{
+#'   \item{dataset}{Name of the dataset}
+#'   \item{column}{Nmae of the column}
+#'   \item{typeof}{Type of the column}
+#'   \item{definition}{Definition of the column}
+#'   ...
+#' }
+#'
+#' @examples
+#' data_dictionary
+
+"data_dictionary"
